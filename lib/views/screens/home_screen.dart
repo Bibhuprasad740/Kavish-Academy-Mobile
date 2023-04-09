c1b6e5d4450d79d3ce6b13d975ce549d8f67a5c1
Bibhu Prasad Sahoo <19btcse09@suiit.ac.in>
2023-04-09 22:44:38 +0530

    added geocoding

--- lib/views/screens/home_screen.dart
+++ lib/views/screens/home_screen.dart
@@ -1,11 +1,14 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:kavish_academy/constants/utils.dart';
 import 'package:kavish_academy/constants/variables.dart';
 import 'package:kavish_academy/views/widgets/custom_textfield.dart';
+
+import '../../constants/colors.dart';
 
 class HomeScreen extends StatefulWidget {
   HomeScreen({Key? key}) : super(key: key);
@@ -21,6 +24,11 @@ class _HomeScreenState extends State<HomeScreen> {
   CameraPosition currentPosition = Variables.initialCam;
   final TextEditingController _desinationLocationController =
       TextEditingController();
+  Completer<GoogleMapController> _completer = Completer();
+  GoogleMapController? _mapController;
+  final geoCodingPlatform = GeocodingPlatform.instance;
+  List<Placemark> userAddress = [];
+  var mapType = MapType.normal;
 
   @override
   void dispose() {
@@ -30,24 +38,41 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   void initState() {
-    getCurrentPosition();
+    getCurrentLocation();
     super.initState();
   }
 
-  Future<void> getCurrentPosition() async {
+  Future<void> getCurrentLocation() async {
     Position? pos = await Utils.getCurrentLocation();
-    print(pos);
-    setState(() {
-      position = pos;
-      currentPosition = CameraPosition(
-        target: LatLng(
-          pos!.latitude,
-          pos.longitude,
-        ),
-        zoom: 18,
-      );
-    });
+    position = pos;
+    setState(() {});
+    LatLng latLng = LatLng(position!.latitude, position!.longitude);
+    currentPosition = CameraPosition(
+      target: latLng,
+      zoom: 16,
+    );
+    _mapController = await _googleMapController.future;
+    _mapController!.animateCamera(
+      CameraUpdate.newCameraPosition(currentPosition),
+    );
+    var address = await geoCodingPlatform.placemarkFromCoordinates(
+        pos!.latitude, pos.longitude);
+    userAddress = address;
+
+    setState(() {});
   }
+
+  void changeMapType() {
+    if (mapType == MapType.normal) {
+      mapType = MapType.hybrid;
+    } else {
+      mapType = MapType.normal;
+    }
+    print(mapType.toString());
+    setState(() {});
+  }
+
+  // void locatePos() async
 
   @override
   Widget build(BuildContext context) {
@@ -62,12 +87,20 @@ class _HomeScreenState extends State<HomeScreen> {
               : GoogleMap(
                   buildingsEnabled: true,
                   myLocationEnabled: true,
-                  mapType: MapType.hybrid,
+                  mapType: mapType,
                   trafficEnabled: true,
-                  padding: const EdgeInsets.all(10),
+                  zoomControlsEnabled: true,
+                  zoomGesturesEnabled: true,
+                  padding: EdgeInsets.only(
+                    bottom: size.height * 0.4,
+                    top: 20,
+                  ),
                   initialCameraPosition: currentPosition,
-                  onMapCreated: (GoogleMapController controller) =>
-                      _googleMapController.complete(controller),
+                  onMapCreated: (GoogleMapController controller) async {
+                    _googleMapController.complete(controller);
+                    await getCurrentLocation();
+                    setState(() {});
+                  },
                   markers: {
                     Marker(
                       markerId: const MarkerId('Source'),
@@ -96,36 +129,61 @@ class _HomeScreenState extends State<HomeScreen> {
                   horizontal: 20,
                   vertical: 18,
                 ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const SizedBox(height: 10),
-                    const Text(
-                      'Hi There!',
-                      style: TextStyle(fontSize: 15),
-                    ),
-                    const SizedBox(height: 10),
-                    const Text(
-                      'Select your destination',
-                      style: TextStyle(fontSize: 20, fontFamily: "Brand-Bold"),
-                    ),
-                    const SizedBox(height: 20),
-                    const Row(
-                      children: [
-                        Icon(Icons.my_location),
-                        SizedBox(width: 10),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [Text('From Your Current Address')],
-                        )
-                      ],
-                    ),
-                    const SizedBox(height: 20),
-                    CustomTextField(
-                      textEditingController: _desinationLocationController,
-                      hintText: 'Search a place',
-                    ),
-                  ],
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const SizedBox(height: 10),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text(
+                            'Hi There!',
+                            style: TextStyle(fontSize: 15),
+                          ),
+                          Container(
+                            padding: const EdgeInsets.all(5),
+                            decoration: BoxDecoration(
+                                color: GlobalColors.accent3.withOpacity(0.2),
+                                borderRadius: BorderRadius.circular(10)),
+                            child: IconButton(
+                              onPressed: changeMapType,
+                              icon: Icon(
+                                Icons.cameraswitch,
+                                color: GlobalColors.accent1,
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                      const SizedBox(height: 10),
+                      const Text(
+                        'Select your destination',
+                        style:
+                            TextStyle(fontSize: 20, fontFamily: "Brand-Bold"),
+                      ),
+                      const SizedBox(height: 20),
+                      userAddress.isEmpty
+                          ? CircularProgressIndicator()
+                          : Row(
+                              children: [
+                                Icon(Icons.my_location),
+                                SizedBox(width: 10),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text('From ${userAddress.first.locality}')
+                                  ],
+                                )
+                              ],
+                            ),
+                      const SizedBox(height: 20),
+                      CustomTextField(
+                        textEditingController: _desinationLocationController,
+                        hintText: 'Search a place',
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
